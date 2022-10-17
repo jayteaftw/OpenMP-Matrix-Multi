0e57981f0cf5f2fec975dded22decb35f186c3e9
JT Vinolus <jvinolus@login1.wave.scu.edu>
2022-10-16 20:22:48 -0700

    simplified

--- run.sh
+++ run.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
+
 make clean && make
 
 echo ""
 echo "Size: 1000x1000 * 1000x1000"
-echo "Sequential"
-./matmult 1000 1000 1000
-echo "Parallel"
-./matmult_omp 1000 1000 1000
+./matmult_omp 1000 1000 1000 | tee 1000x1000*1000x1000.log
 echo ""
 
 echo "Size: 1000x2000 * 2000x5000"
-echo "Sequential"
-./matmult 1000 2000 5000
-echo "Parallel"
-./matmult_omp 1000 2000 5000
+./matmult_omp 1000 2000 5000 | tee 1000x2000*2000x5000.log
 echo ""
 
 echo "Size: 9000x2000 * 2000x3750"
-echo "Sequential"
-./matmult 9000 2500 3750
-echo "Parallel"
-./matmult_omp 9000 2500 3750
+./matmult_omp 9000 2500 3750 | tee 9000x2000*2000x3750.log
 echo ""
